d0b8e5f6cc3dfbb8cb50ef18df82eb6fd5a989cf
Jake O'Shaughnessy <jake.oshaughnessy@ucdconnect.ie>
2021-04-28 17:18:25 +0100

    Added some move validation

--- play_game.c
+++ play_game.c
@@ -7,40 +7,141 @@ bool isValid(enum othelloColour boardArr[SIZE][SIZE], player Player,int row, int
     player currentPlayer = Player;
     bool valid = false;
     int i, j;
-    // for (int i = 0; i < SIZE; ++i) {
-    //    for (int j = 0; j < SIZE; ++j) {
+    int result =0;
 
 //generic checks
     if (boardArr[row][col] == BLACK || boardArr[row][col] == WHITE) {//if move already contains a player
         valid = false;
-    } else if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) {//if move is off the board
+    }
+    if (row < 0 || row >= SIZE || col < 0 || col >= SIZE) {//if move is off the board
         valid = false;
     }
-        //check horizontally right
-        else if( (row+1) >= SIZE || (col+1) != EMPTY || (row+1) == currentPlayer.colour){//if position to right is off the board, or empty, or has a disc of same colour
-              valid = false;
-           }
     else {
         valid = true;
     }
 
     while (valid == true) {
-        //check left
+        //check horizontally left
         //needs to be opponants colour
         if (col > 1 && (boardArr[row][col - 1] != currentPlayer.colour || boardArr[row][col - 1] != EMPTY)) {
 
             for (j = col - 2; j >= 0; j--) {
                 if (boardArr[row][j] == currentPlayer.colour) {
                     result += 1;
-                    count += n - j - 1;
+                   // count += n-j-1;
+                    valid = true;
                     break;
                 }
             }
-            if (board[m][j] == NONE) {
+            if (boardArr[row][j] == EMPTY){
                 break;
             }
         }
+        //check horizontally right
+        //if positon not currentPlayer.colour and not blank i.e. opponents colour
+        if ( col < SIZE-2 && (boardArr[row][col + 1] != currentPlayer.colour || boardArr[row][col - 1] != EMPTY)) {
+            for ( j=col+2; j < SIZE; j++ ) {
+                if ( boardArr[row][j] == currentPlayer.colour ) {
+                    result += 2;
+                    valid = true;
+                   // count += j-n-1;
+                    break;
+                }
+                if ( boardArr[row][j] == EMPTY ) {
+                    break;
+                }
+            }
+        }
+        //check vertically up
+        if ( row > 1 && (boardArr[row-1][col] != currentPlayer.colour || boardArr[row][col - 1] != EMPTY) ) {
+            for ( i=row-2; i >= 0; i-- ) {
+                if ( boardArr[i][col] == currentPlayer.colour) {
+                    result += 4;
+                    valid = true;
+                    //count += m-i-1;
+                    break;
+                }
+                if ( boardArr[i][col] == EMPTY ) {
+                    break;
+                }
+            }
+        }
+        //check vertically down
+        if ( row < SIZE-2 && (boardArr[row + 1][col] != currentPlayer.colour || boardArr[row][col - 1] != EMPTY) ) {
+            for ( i=row+2; i < SIZE; i++ ) {
+                if ( boardArr[i][col] == currentPlayer.colour ) {
+                    result += 8;
+                   // count += i-m-1;
+                    valid = true;
+                    break;
+                }
+                if ( boardArr[i][col] == EMPTY) {
+                    break; }
+            }
+        }
+        //check up diagonal left
+        if ( row > 1 && col > 1 && (boardArr[row-1][col-1] != currentPlayer.colour || boardArr[row][col - 1] != EMPTY) ) {
+            for ( i=row-2, j=col-2;  i>=0 && j>=0;  i--, j-- ) {
+                if ( boardArr[i][j] == currentPlayer.colour ) {
+                    result += 16;
+                    //count += n-j-1;
+                    valid = true;
+                    break; }
+                if ( boardArr[i][j] == EMPTY    ) {
+                    break;
+                }
+            }
+        }
+        //check down diagonal left
+        if ( row < SIZE-2 && col > 1 && (boardArr[row+1][col-1] != currentPlayer.colour || boardArr[row][col - 1] != EMPTY) ) {
+            for ( i=row+2, j=col-2;  i<SIZE && j>=0;  i++, j-- ) {
+                if ( boardArr[i][j] == currentPlayer.colour ) {
+                    result += 32;
+                    //count += n-j-1;
+                    valid = true;
+                    break;
+                }
+                if ( boardArr[i][j] == EMPTY ) {
+                    break;
+                }
+            }
+        }
+        //check down diagonal left
+        if ( row > 1 && col < SIZE-2 && (boardArr[row+1][col-1] != currentPlayer.colour || boardArr[row][col - 1] != EMPTY) ) {
+            for ( i=row-2, j=col+2;  i>=0 && j<SIZE;  i--, j++ ) {
+                if ( boardArr[i][j] == currentPlayer.colour ) {
+                    result += 64;
+                   // count += j-n-1;
+                    valid = true;
+                    break; }
+                if ( boardArr[i][j] == EMPTY    ) {
+                    break;
+                }
+            }
+        }
+        //check down diagonal right
+        if ( row < SIZE-2 && col < SIZE-2 && (boardArr[row+1][col+1] != currentPlayer.colour || boardArr[row][col - 1] != EMPTY) ) {
+            for ( i=row+2, j=col+2;  i < SIZE && j < SIZE;  i++, j++ ) {
+                if ( boardArr[i][j] == currentPlayer.colour ) {
+                    result += 128;
+                   // count += j-n-1;
+                    valid = true;
+                    break;
+                }
+                if ( boardArr[i][j] == EMPTY    ) {
+                    break;
+                }
+            }
+        }
+
+
+
+
     }
+
+
+
+    return valid;
 
 
 
@@ -48,20 +149,9 @@ bool isValid(enum othelloColour boardArr[SIZE][SIZE], player Player,int row, int
 
 
         // }
-    }
-
-    /*    for ( j=y-2; j >= 0; j-- ) {
-                   if ( boardArr[x][j] == currentPlayer.colour ) {
-                       result += 1;
-                       count += n-j-1;
-                       break; }
-               }
-                   if ( board[m][j] == NONE ) {
-                       break;
-                   }
-               }
-               */
 
 
 
-}
+
+
+
